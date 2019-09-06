b07100f87713ef03892adf6a153b1468111d54a6
TurtleWolfe <JonPohlner@gmail.com>
2019-09-06 17:17:17 -0400

    Added Chart and image of CSS issue with &:Hover

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
     import Timer from './Pomodoro/Timer';
     import Coins from './Coins/Coins';
     import Notes from './Notes/Notes';
+    import Chart from './Chart/Chart';
 // Layout Components
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
@@ -19,10 +20,29 @@ class App extends Component {
     
     // The first time we load the notes1...
           this.state = {
-    notes: notes1
+    notes: notes1,
+    chartType: 'line'
           };
+
+          this.columns = [
+            ['BTC', 3000, 6000, 10000, 15000, 13000, 11000],
+            ['ETH', 2000, 3000, 5000, 4000, 3000, 940],
+            ['XRP', 100, 200, 300, 500, 400, 300],
+          ];          
+        }
+
+    setBarChart = () => {
+          this.setState({
+    chartType: 'bar'
+          });
         }
     
+    setLineChart = () => {
+          this.setState({
+    chartType: 'line'
+          });
+        }        
+
     componentDidMount() {
     // After 10 seconds (10000 milliseconds) we concatenate our       
           // data with notes2...
@@ -43,6 +63,16 @@ class App extends Component {
             <Timer />
             <Coins />
             <Notes notes={this.state.notes} />
+            <Chart
+              columns={this.columns} 
+              chartType={this.state.chartType} 
+            />
+
+            <p>
+              Chart Type
+              <button onClick={this.setBarChart}>Bar</button>
+              <button onClick={this.setLineChart}>Line</button>
+            </p>
       </Content>
       <Footer />
     </div>
